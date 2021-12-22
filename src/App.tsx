06b34e221b9840a6e9b3cf9fbd806c47b0e2ce6b
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2021-12-22 22:45:12 +0900

    gh-pages_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   );
 }
