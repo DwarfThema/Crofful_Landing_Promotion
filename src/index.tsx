bd5fcb9428dce4bce17a73e297d58843c5811540
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-01-11 12:06:29 +0900

    emoji fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const GlobalStyled = createGlobalStyle`
 @import url('https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css');
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Roboto+Mono:wght@300&display=swap');
 
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
